b49211f4e9548d31838afed7c3d7301a35eaab4d
Ignacio <ignacionweppler@gmail.com>
2025-10-16 16:23:40 -0300

    fix import

--- front/src/components/ui/CustomSelect.tsx
+++ front/src/components/ui/CustomSelect.tsx
@@ -1,7 +1,6 @@
 import { useField } from "formik";
 import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
 interface FormikShadcnSelectProps {
   name: string;
   label: string;
